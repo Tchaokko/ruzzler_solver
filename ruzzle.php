3c2e99241f90edcf11c565a6a78b30f75e909805
jerome gangneux <jerome@MacBook-Pro-de-jerome.local>
2013-07-24 11:22:48 +0200

    French words database & SQL optimization

--- ruzzle.php
+++ ruzzle.php
@@ -1,7 +1,26 @@
 <?php
-header('Content-Type: text/html; charset=UTF-8');
-error_reporting(E_ALL);
-set_time_limit(30);
+
+    header('Content-Type: text/HTML; charset=utf-8');
+    header('Content-Encoding: none;');
+    session_start();
+    ob_end_flush();
+    ob_start();
+    set_time_limit(0);
+    error_reporting(0);
+
+    // while (true) {
+    //     sleep (1);
+    //     echo 'ok<br />';
+    //     ob_flush();
+    //     flush();
+    // }
+
+    // exit;
+
+    // header('Content-Type: text/html; charset=UTF-8');
+    // error_reporting(E_ALL);
+    // set_time_limit(100);
+    // ob_start();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
@@ -12,27 +31,25 @@ set_time_limit(30);
 
 
     <style>
-    *{ 
-    font-family: "Courier New", Courier, monospace;
-      }
-     .board {
-     width: 250px;
-    position: fixed;
-    top : 0;
-    right : 0;
-        }
-     .board span {
-     display: inline-block;
-     height: 50px;
-     width: 50px;
-     text-align: center;
-     background: red;
-     border: solid 3px black;
-    font-size: 2.5em;
-    font-weight: bold;
-    border-radius: 10px;
-    margin: 2px;
-}
+    * { font-family: "Courier New", Courier, monospace; }
+    .board {
+        width: 250px;
+        position: fixed;
+        top : 0;
+        right : 0;
+    }
+    .board span {
+        display: inline-block;
+        height: 50px;
+        width: 50px;
+        text-align: center;
+        background: red;
+        border: solid 3px black;
+        font-size: 2.5em;
+        font-weight: bold;
+        border-radius: 10px;
+        margin: 2px;
+    }
     </style>
   </head>
   <body>
@@ -52,19 +69,26 @@ set_time_limit(30);
 <?php
 
 define('MAX_WORD', 4);
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'english_words');
+define('DEBUG', false);
 
 function d($string)
 {
-    if (is_array($string)){
-        $string = 'array : ' . implode(',', $string);
+    if (DEBUG) {
+        if (is_array($string)){
+            $string = 'array : ' . implode(',', $string);
+        }
+        echo $string . '<br/>';
     }
- //  echo $string . '<br/>';    
 }
 
-$bdd = mysql_connect("localhost", "root", "wfta325aser");
+$bdd = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 mysql_query("SET NAMES utf8");
 
-if (!mysql_select_db("word", $bdd)) {
+if (!mysql_select_db(DB_DATABASE, $bdd)) {
     die('ERROR CONNECTING TO THE DB');
 }
 
@@ -82,7 +106,8 @@ if (
         '<span>' . implode('</span><span>', str_split($_POST['line3'])) . '</span>' .
         '<span>' . implode('</span><span>', str_split($_POST['line4'])) . '</span>' .
         '</div>';
-    $sql = 'SELECT word FROM french';
+
+    $sql = 'SELECT * FROM french WHERE size > 5 AND size < 9 ORDER BY size DESC';
     $table = mysql_query($sql, $bdd);
     $tableaux =  array();
 
@@ -95,22 +120,20 @@ if (
 
     function check_around($tableaux, $mot, $nbr, $save, $pos_x, $pos_y)
     {
-        // if ($nbr > 3) {
-        //     die('MAX');
-        // }
-
         $nbr += 1;
         $pos = array(
             array(1, 0), array(0, 1), array(0, -1), array(-1, 0), array(1, 1), array(1, -1), array(-1, 1), array(-1, -1)
         );
+
         if ($nbr == count($mot)) {
-            //var_dump($mot);
             d('end of word');
             return true;
         }
+
         d('start');
         d($mot);
         d($mot[$nbr]);
+
         for ($count = 0; $count < 8; $count++) {
             d('for ' . $pos_x . $pos_y);
             d($pos[$count]);
@@ -127,12 +150,12 @@ if (
                 d('inbound');
                 if ($tableaux
                     [$pos_x + $pos[$count][0]]
-                  [$pos_y + $pos[$count][1]] == $mot[$nbr]
+                    [$pos_y + $pos[$count][1]] == $mot[$nbr]
                 ) {
                     d('yay');
                     if (empty($save[$interm_x][$interm_y])) {
                         d('position test');
-                        $save[$interm_x][$interm_y] = true;	
+                        $save[$interm_x][$interm_y] = true;
                         if (check_around($tableaux, $mot, $nbr, $save, $interm_x, $interm_y)) {
                             d('go');
                             return true;
@@ -147,23 +170,19 @@ if (
     function algo($tableaux, $mot, $nbr, $save)
     {
         for ($count2 = 0; $count2 < MAX_WORD; $count2++)  {
-            for ($count3 = 0; $count3 < MAX_WORD; $count3++){
-                if ($tableaux[$count2][$count3] == $mot[$nbr]){
+            for ($count3 = 0; $count3 < MAX_WORD; $count3++) {
+                if ($tableaux[$count2][$count3] == $mot[$nbr]) {
                     $save[$count2][$count3] = true;
-                    if (check_around($tableaux, $mot, $nbr, $save, $count2, $count3)){
+                    if (check_around($tableaux, $mot, $nbr, $save, $count2, $count3)) {
                         return true;
                     }
                 }
             }
         }
     }
-    $block = 0;
+
     $save = array();
     while ($data = mysql_fetch_array($table)) {
-        $block++;
-        //      if ($block > 5)
-        //        die('OK');
-        //  echo $data['word'];
         $mot = str_split($data['word']);
             if (algo($tableaux, $mot, 0, $save)) {
                 if ($count > 100) {
@@ -171,6 +190,8 @@ if (
                 }
                 $result[$count] = $data['word'];
                 echo $result[$count] . '<br>';
+                ob_flush();
+                flush();
                 $count++;
             }
     }
