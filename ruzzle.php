90411f628d4e49d160ab72ab0f3a19ca8ec06fb1
Tchaoko <roubertoux.leo@gmail.com>
2013-07-26 14:09:31 +0200

    Version 0.2

--- ruzzle.php
+++ ruzzle.php
@@ -6,8 +6,9 @@ define('MAX_SIZE', 9);
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'english_words');
+define('DB_DATABASE', 'french_words');
 define('AUTO_SCROLL', false);
+define('MAX_LETTER', 26);
 
 header('Content-Type: text/HTML; charset=utf-8');
 header('Content-Encoding: none;');
@@ -15,7 +16,7 @@ session_start();
 ob_end_flush();
 ob_start();
 set_time_limit(0);
-error_reporting(0);
+error_reporting(-1);
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -59,6 +60,30 @@ error_reporting(0);
         border-radius: 10px;
         margin: 2px;
     }
+
+    p {
+        width: 50px;
+    }
+
+    .submit{
+        bottom: 50px;
+    }
+
+    .table{
+        position: absolute;
+        right:0px;
+        top: 0px;
+        width: 250px;
+   }
+
+    .table td{
+        border: 2px black solid;
+        text-align: center;
+        width: 50px;
+        height: 50px;
+        margin: 2px;
+        border-radius: 10px;
+    }
     </style>
     <?php if (AUTO_SCROLL) : ?>
     <script>
@@ -70,17 +95,40 @@ error_reporting(0);
     <?php endif ?>
   </head>
   <body onLoad="pageScroll()">
-
+  
 <?php if (empty($_POST)) : ?>
+      <table class="table">
+        <tr>
+            <td>a</td>
+            <td>b</td>
+            <td>c</td>
+            <td>d</td>
+        </tr>
+          <tr>
+            <td>e</td>
+            <td>f</td>
+            <td>g</td>
+            <td>h</td>
+        </tr>
+          <tr>
+            <td>i</td>
+            <td>j</td>
+            <td>k</td>
+            <td>l</td>
+        </tr>
+          <tr>
+            <td>m</td>
+            <td>n</td>
+            <td>o</td>
+            <td>p</td>
+        </tr>
+    </form>
     <h1>Ruzzle Finder</h1>
     <form method="post">
     <label for="pwd">Votre grille</label>
-    <p><input type="text" name="line1" id="pwd" maxlength="4" size="4"/><br></p>
-    <p><input type="text" name="line2" id="pwd" maxlength="4" size="4"/><br></p>
-    <p><input type="text" name="line3" id="pwd" maxlength="4" size="4"/><br></p>
-    <p><input type="text" name="line4" id="pwd" maxlength="4" size="4"/><br></p>
-    <p><input type="submit" name="envoyer"/></p>
-    </form>
+    <p><input type="text" name="line1" id="pwd" maxlength="16" size="16"/><br></p>
+    <p><input type="submit" name="envoyer" class="submit"/></p>
+
 <?php else : ?>
 
 <?php
@@ -107,27 +155,25 @@ if (!mysql_select_db(DB_DATABASE, $bdd)) {
 }
 
 if (
-    !empty($_POST['line1']) &&
-    !empty($_POST['line2']) &&
-    !empty($_POST['line3']) &&
-    !empty($_POST['line4'])
+    !empty($_POST['line1'])
 ) {
-
     echo
         '<div class="board">' .
         '<span>' . implode('</span><span>', str_split($_POST['line1'])) . '</span>' .
-        '<span>' . implode('</span><span>', str_split($_POST['line2'])) . '</span>' .
-        '<span>' . implode('</span><span>', str_split($_POST['line3'])) . '</span>' .
-        '<span>' . implode('</span><span>', str_split($_POST['line4'])) . '</span>' .
         '</div>
         <div class="words">';
 
     $sql = 'SELECT * FROM french WHERE size > ' . MIN_SIZE . ' AND size < ' . MAX_SIZE; // . ' ORDER BY size DESC';
     $table = mysql_query($sql, $bdd);
     $tableaux =  array();
+    $interm_tab = str_split($_POST['line1']);
+    $start = 0;
+    $end = MAX_WORD;
 
     for ($count = 0; $count < MAX_WORD; $count++) {
-        $tableaux[$count] = str_split($_POST['line' . ($count + 1)]);
+        $tableaux[$count] = substr($_POST['line1'], $start, $end);
+        $tableaux[$count] = str_split($tableaux[$count]);
+        $start += 4;
     }
 
     $count = 0;
@@ -233,11 +279,57 @@ if (
             return true;
         }
     }
+    $tab = [
+        "a" => 1,
+        "b" => 1,
+        "c" => 3,
+        "d" => 2,
+        "e" => 1,
+        "f" => 4,
+        "g" => 2,
+        "h" => 1,
+        "i" => 1,
+        "j" => 1,
+        "k" => 1,
+        "l" => 2,
+        "m" => 2,
+        "n" => 1,
+        "o" => 1,
+        "p" => 3,
+        "q" => 8,
+        "r" => 1,
+        "s" => 1,
+        "t" => 1,
+        "u" => 1,
+        "v" => 5,
+        "x" => 1,
+        "y" => 1,
+        "z" => 1
+     ];
 
-    sort($result);
+    function value_point($string, $tab){
+        $char_tab = str_split($string);
+        $value = 0;
+        foreach ($char_tab as $key => $letter) {
+        //echo 'test <br/>'; 
+            foreach ($tab as $base => $nbr) {
+                if ($base == $letter){
+                    $value += $nbr;
+                }
+            }
+        }
+        return $value;
+    }
+
+    $result_with_score = [];
+    for($count2 = 0; $count2 < $count; $count2++){
+        $value = value_point($result[$count2], $tab);
+        $result_with_score[$result[$count2]] = $value;
+    }
+    arsort($result_with_score);
     $prev = "";
-    foreach ($result as $value) {
-        if (similar($prev, $value)) {
+    foreach ($result_with_score as $value => $word) {
+        if (similar($prev, $word)) {
             echo " - ";
         }
         else {
